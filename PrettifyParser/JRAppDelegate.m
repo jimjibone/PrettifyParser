1daa6f55156c9b80af81cee74d582271f3637cc3
James Reuss <James@James-Hack-Pro.local>
2012-11-29 13:07:51 +0000

    Initial Commit

---
+++ PrettifyParser/JRAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  JRAppDelegate.m
+//  PrettifyParser
+//
+//  Created by James Reuss on 29/11/2012.
+//  Copyright (c) 2012 James Reuss. All rights reserved.
+//
+
+#import "JRAppDelegate.h"
+
+@implementation JRAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ PrettifyParser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PrettifyParser
+//
+//  Created by James Reuss on 29/11/2012.
+//  Copyright (c) 2012 James Reuss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
